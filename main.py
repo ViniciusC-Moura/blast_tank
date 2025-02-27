e80865c6bc68b00f1230b42500de4a1e4993ce12
ViniciusC-Moura <vinicius.cavalcanti@escolar.ifrn.edu.br>
2025-02-27 20:45:13 -0300

    Mudança das imagens e incrementação das classes

---
+++ main.py
@@ -0,0 +1,136 @@
+import pygame
+import math
+
+
+pygame.init()
+screen = pygame.display.set_mode((880, 650))
+clock = pygame.time.Clock()
+running = True
+
+
+class Bala:
+    def __init__(self, x, y, vx, vy, dono, cor_bala):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.dono = dono
+        self.cor_bala = cor_bala
+
+    def update(self):
+        self.x += self.vx
+        self.y += self.vy
+        return 0 <= self.x <= 880 and 0 <= self.y <= 650
+
+    def draw(self):
+        pygame.draw.circle(screen, self.cor_bala, (int(self.x), int(self.y)), 5)
+
+class Tanque:
+    def __init__(self, x, y, cor, teclas, image):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.cor = cor
+        self.teclas = teclas
+        self.movendo = False
+        self.sentido_rotacao = True  #True: horario // False: anti-horario
+        self.vidas = 3
+        self.balas = 3
+        self.ultimo_reload = pygame.time.get_ticks()
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.vx = 0
+        self.vy = 0
+
+    def update(self):
+        if self.movendo:
+            rad = math.radians(self.angulo)
+            novo_x = self.x + math.cos(rad) * 3
+            novo_y = self.y + math.sin(rad) * 3
+
+            if 0 <= novo_x <= 880 - 64:
+                self.x = novo_x
+            if 0 <= novo_y <= 650:
+                self.y = novo_y
+
+        if self.balas < 3 and pygame.time.get_ticks() - self.ultimo_reload > 2000:
+            self.balas += 1
+            self.ultimo_reload = pygame.time.get_ticks()
+
+        if self.sentido_rotacao:
+            self.angulo -= 2
+        else:
+            self.angulo += 2
+
+    def draw(self):
+        imagem_rodada = pygame.transform.rotate(self.image, -self.angulo)
+        rect_rodado = imagem_rodada.get_rect(center=(self.x, self.y))
+        screen.blit(imagem_rodada, rect_rodado.topleft)
+
+    def atirar(self, balas):
+        if self.balas > 0:
+            rad = math.radians(self.angulo)
+            bala_x = self.x + math.cos(rad) * 32
+            bala_y = self.y + math.sin(rad) * 32
+            balas.append(Bala(bala_x, bala_y, math.cos(rad) * 5, math.sin(rad) * 5, self, self.cor))
+            if self.balas == 3:
+              self.ultimo_reload = pygame.time.get_ticks()
+            self.balas -= 1
+
+
+    def checar_colisao(self, balas):
+        tank_rect = self.image.get_rect(center=(self.x, self.y))
+        for bala in balas[:]:
+            bala_rect = pygame.Rect(bala.x - 5, bala.y - 5, 10, 10)
+            if bala_rect.colliderect(tank_rect) and bala.dono != self:
+                balas.remove(bala)
+                self.vidas -= 1
+                if self.vidas <= 0:
+                    return True
+        return False
+
+#==================# IMAGENS #===========================#
+muni_blue = pygame.image.load("muni_blue.png")
+muni_red = pygame.image.load("muni_red.png")
+coracao_blue = pygame.transform.scale(pygame.image.load("coracao_blue.png"), (52, 48))
+coracao_red = pygame.transform.scale(pygame.image.load("coracao_red.png"), (52, 48))
+#========================================================#
+
+
+
+def main():
+    jogando = False
+
+    running = True
+    
+    while running:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONUP and not jogando:
+              if botao_rect.collidepoint(pygame.mouse.get_pos()):
+                jogando = True
+
+        if not jogando:
+          screen.blit(pygame.image.load("fundo_inicio.jpg"), (0,0))
+
+          botao_rect = pygame.Rect(355, 480, 140, 60)
+          pygame.draw.rect(screen, (181, 181, 181), botao_rect, 0, 15)
+
+          fonte = pygame.font.Font('freesansbold.ttf', 36)
+          texto_play = fonte.render('Play', True, (194, 0, 0), (181, 181, 181))
+          textRect = texto_play.get_rect()
+          textRect.center = (425, 510)
+          screen.blit(texto_play, textRect)
+
+        else:
+          screen.blit(pygame.image.load("fundo_game.jpg"), (0,0))
+
+        pygame.display.flip()
+        clock.tick(60)
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
